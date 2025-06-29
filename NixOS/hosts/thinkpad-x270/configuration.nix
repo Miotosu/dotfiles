c86f4624d4d8bed4cf9d622cf400059235b38281
Koda Katsu <116800966+Miotosu@users.noreply.github.com>
2025-06-29 13:06:39 +0200

    asdasd

--- NixOS/hosts/thinkpad-x270/configuration.nix
+++ NixOS/hosts/thinkpad-x270/configuration.nix
@@ -81,8 +81,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
-  home-manager.users.klouwer = import ../../home-manager/home.nix {
-    inherit pkgs inputs;
-  };
+  home-manager.users.klouwer = import ../../home-manager/home.nix;
 
 }
