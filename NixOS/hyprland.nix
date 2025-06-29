be7adaba44fbd3074ed29225ada3fa1f600fcd06
Koda Katsu <116800966+Miotosu@users.noreply.github.com>
2025-06-29 13:23:30 +0200

    sadasd

--- NixOS/hyprland.nix
+++ NixOS/hyprland.nix
@@ -4,13 +4,10 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    settings = {
-      # Execute your programs on startup
-      exec-once = "waybar";
-
-      # Set a keybind for wofi (Mod + D)
-      bind = "SUPER, D, exec, wofi --show drun";
-    };
+    extraConfig = ''
+      exec-once = waybar
+      bind = SUPER, D, exec, wofi --show drun
+    '';
   };
 
   xdg.portal = {
