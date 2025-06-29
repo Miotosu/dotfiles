e5f0c5d53f58e6c5896e0b0d00c60709cbd4f8aa
Koda Katsu <116800966+Miotosu@users.noreply.github.com>
2025-06-29 13:48:15 +0200

    asd

--- NixOS/hyprland.nix
+++ NixOS/hyprland.nix
@@ -3,28 +3,8 @@
 {
   programs.hyprland = {
     enable = true;
-    withUWSM = true;
     xwayland.enable = true;
   };
-
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    pyprland
-    hyprpicker
-    hyprcursor
-    hyprlock
-    hypridle
-    hyprpaper
-
-    inputs.wezterm.packages.${pkgs.system}.default
-    kitty
-    cool-retro-term
-
-    starship
-    helix
-
 
   xdg.portal = {
     enable = true;
